dfeef0ccaac893446436200c19b20279605115ab
dhirajmistry <dhirajmistry1368@gmail.com>
2026-03-16 13:11:22 +0530

    Initial commit: setup Next.js project for NovaUI AI web design agent

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/sonner";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+             <TooltipProvider>
+              {children}
+          <Toaster richColors/>
+             </TooltipProvider>
       </body>
     </html>
   );
